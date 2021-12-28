c4cda91b1713d9ece13cc94f62a8af371688c9c5
Priyanka Salunke <salunkeps99@gmail.com>
2021-12-27 20:35:50 -0700

    Create multipart api

--- multipart.py
+++ multipart.py
@@ -1,14 +1,14 @@
-#
-
+# multipart api
 import asyncio
 import subprocess
+import fileinput
 import os
 from subprocess import PIPE
 from ast import literal_eval
 
 def main():
     source_bucket_name = "sourcebucket"
-    source_object_name = "object1"
+    source_object_name = "object14"
     print(source_bucket_name + " " + source_object_name)
     command = "aws s3api create-multipart-upload --bucket " + source_bucket_name + " --key " + source_object_name
     out = subprocess.check_output(command, shell=True)
@@ -18,18 +18,31 @@ def main():
     print("Create multipart upload : {}".format(final_res))
     upload_id = final_res.get("UploadId")
     print(upload_id)
+
+
     # Upload part
     part_no = 5
     etag_dict = {}
     files = subprocess.check_output("cat etag.json", shell=True)
+
+    #file = os.open('demo_object.txt',os.O_RDONLY)
+    #tagset = os.read(file, os.path.getsize(file))
     #files = files.decode('utf-8')
     #print(files)
     for i in range(1, part_no+1):
-      command2 = "aws s3api upload-part --bucket " + source_bucket_name + " --key " + source_object_name + " --upload-id '" + upload_id + "' --part-number "+str(i) + " --body 'xaa'"
+     with open("demo_object.txt","r") as f1:
+      ff2 = f1.read()
+      print("****file : ",ff2)
+      command2 = "aws s3api upload-part --bucket " + source_bucket_name + " --key " + source_object_name + " --upload-id '" + upload_id + "' --part-number "+str(i) + " --body file://demo_object.txt"
+      print("Command : {}".format(command2))
+      status = os.system(command2)
+      print("**status : {}",status)
       out2 = subprocess.check_output(command2, shell=True)
       res2 = out2.decode('utf-8')
-      etag_dict[i] = res2
-    print("Upload part : {}".format(res2))
+      etag = res2.get("ETag")
+      etag_dict[i] = etag
+    print("Upload part : {}".format(etag_dict))
+
    
     # complete multipart
     etag_str = "<CompleteMultipartUpload>"
