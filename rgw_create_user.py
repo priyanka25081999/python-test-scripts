7449ccaf74bb052b73737dfe9350fd0ec3b551ec
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-03-24 09:08:38 +0530

    cosmetic changes

--- rgw_create_user.py
+++ rgw_create_user.py
@@ -1,16 +1,15 @@
-// Script to create a normal user using rados admin API
-// use of rgwadmin python library
+# Script to create a normal user using rados admin API
+# use of rgwadmin python library
 
 from rgwadmin import RGWAdmin
 
-// establish a connection
+# establish a connection
 rgw = RGWAdmin(access_key='__YOUR_ACCESS_KEY__', secret_key='__YOUR_SECRET_KEY__', server='10.230.245.6:8000', secure=False, verify=False)
 
-// create a user
+# create a user
 rgw.create_user(
     uid='prog-user',
     display_name='program user')
-
 print("User created successfully")
 
 # get users
